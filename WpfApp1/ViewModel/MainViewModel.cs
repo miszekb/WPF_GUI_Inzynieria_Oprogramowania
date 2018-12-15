a4db4710c905973b3568814cc350c002edcfaafa
Marcin <210211@edu.p.lodz.pl>
2018-12-15 11:09:04 +0100

    Wstępny MVVM

---
+++ WpfApp1/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Model;
+
+namespace WpfApp1.ViewModel
+{
+    internal class MainViewModel : ViewModelBase, IDataErrorInfo
+    {
+        public string this[string columnName] => throw new NotImplementedException();
+
+        public string Error => throw new NotImplementedException();
+    }
+}
